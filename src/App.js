238f1abd034947a88728c82e6660fbeb45c010f5
chiraze <ajroudichiraze@gmail.com>
2023-08-02 22:36:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [person, setperson ]=useState({fullname:"your name",profession:"your profession", show:false})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <button onClick={()=>setperson({...person, show:true})}>Show</button>
+      {
+        person.show==true?
+        (
+          <>
+          <h1>{person.fullname}</h1>
+          <h2>{person.profession}</h2>
+          </>
+        ):null
+      }
+      
+     
+    
+      </div>)
+     }
+  
+      
+
 
 export default App;
